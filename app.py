419300bbfe5bacabb7bfc1ebf0ab54f7c17c5a54
IamJeevz <jeevansaju1@outlook.com>
2024-06-06 12:10:19 +0530

    usr pass

--- app.py
+++ app.py
@@ -16,9 +16,12 @@ logger = logging.getLogger(__name__)
 app = FastAPI()
 
 class Resource(BaseModel):
-    name: str
+    username: str
+    password: str
 
-REQUIRED_USER_AGENT = "jeevan"
+REQUIRED_USER_AGENT = "ptpl"
+REQ_USERNAME = "prudent"
+REQ_PASSWORD = "Admin"
 
 @app.middleware("http")
 async def log_requests(request: Request, call_next):
@@ -26,7 +29,11 @@ async def log_requests(request: Request, call_next):
 
     # Retrieve request parameters
     query_params = dict(request.query_params)
-    body = await request.json() if request.method in ("POST", "PUT") else None
+    headers = dict(request.headers)  # Access headers directly using request.headers
+    try:
+        body = await request.json() if request.method in ("POST", "PUT") else None
+    except:
+        body = None
 
     response = await call_next(request)
     process_time = time.time() - request_time
@@ -34,6 +41,7 @@ async def log_requests(request: Request, call_next):
     log_message = (
         f"{request.method} {request.url} - "
         f"Query params: {json.dumps(query_params)} - "
+        f"Headers: {json.dumps(headers)} - "
         f"Body: {json.dumps(body)} - "
         f"Status code: {response.status_code} - "
         f"Processing time: {process_time:.4f} seconds"
@@ -44,12 +52,21 @@ async def log_requests(request: Request, call_next):
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello World"}
+    return {"message": "Hello, Welcome to Prudent"}
 
 @app.post("/api/v1/resource")
 def create_resource(resource: Resource, user_agent: Optional[str] = Header(None)):
     if user_agent != REQUIRED_USER_AGENT:
         raise HTTPException(status_code=400, detail="Invalid User-Agent header")
+        
+    if resource.username != REQ_USERNAME:
+        raise HTTPException(status_code=400, detail="Invalid username")
+        
+    if resource.password != REQ_PASSWORD:
+        raise HTTPException(status_code=400, detail="Invalid password")
+
+    # Log username and password
+    logger.info(f"Username: {resource.username}, Password: {resource.password}")
 
     return {
         "message": "Success",
